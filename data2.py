3acb5ae6cbcec6c9cbeeb561678fbe21a1700c0b
brianna-341 <158204317+brianna-341@users.noreply.github.com>
2024-02-06 12:28:09 -0500

    help2

---
+++ data2.py
@@ -0,0 +1,38 @@
+
+
+
+
+""" values = [1,2.23,5,7,2,30,15] """
+""" print(values)
+for i in values:
+    print(i) """
+""" print(values[2])
+print(values[6]) """
+
+#challenge 1 redo
+""" x = "hello how are you"
+y= x.split( )
+z = y[0]
+print(y)
+print(z) """
+
+#challenge 2  redo
+""" zoo = input("zoo")
+house = input("house")
+madlibs =  """
+
+""" x = "test"
+print(f"hello {x}") """
+""" 
+temp = 50
+if temp > 68:
+    print('warm')
+elif temp == 68:
+    print('perfect')
+else:
+    print('cold') """
+""" 
+#challenge 1
+values = [0,2,4,6,8,10]
+if values : [3,5,7,11]
+    print(odd) """
